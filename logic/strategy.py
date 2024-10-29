b3afc38b10f669ead85a6930eda283c36dc201b4
Alexey <alexeyvasin@gmail.com>
2024-10-29 22:34:12 +0300

    Added Semaphore(20)

--- logic/strategy.py
+++ logic/strategy.py
@@ -9,43 +9,54 @@ from t_invest_api.info_queries import get_instruments, get_tech_analysis
 
 
 
-async def is_overbought(instrument: dict) -> dict | None:
-    now_time = datetime.now(timezone.utc) # current  time
-    three_hours_ago = now_time - timedelta(1) # time several days ago
+async def is_overbought(instrument: dict, semaphore) -> None:
+    async with semaphore:
+        now_time = datetime.now(timezone.utc) # current  time
+        three_hours_ago = now_time - timedelta(1) # time several days ago
 
-    # receive averages
-    rsi = await asyncio.gather(
-        get_tech_analysis(
-        instrument_uid=instrument['uid'],
-        to_time= now_time.isoformat(timespec='milliseconds').replace('+00.00', 'Z'),
-        from_time=three_hours_ago.isoformat(timespec='milliseconds').replace('+00.00', 'Z'),
-        length=14
-    )
-    )
-    if not rsi:
-        print('*instrument', instrument)
-        print('*rsi.text', rsi[0].text)
-        print('*******************')
-        return
-    # print(instrument)
-    # print(json.loads(rsi[0].text)['technicalIndicators'])
-    if res := json.loads(rsi[0].text)['technicalIndicators']:
-        if int(res[-1]['signal']['units']) > 70:
-            return res
+        # receive averages
+        rsi_indicator = await asyncio.gather(
+            get_tech_analysis(
+            instrument_uid=instrument['uid'],
+            to_time= now_time.isoformat(timespec='milliseconds').replace('+00.00', 'Z'),
+            from_time=three_hours_ago.isoformat(timespec='milliseconds').replace('+00.00', 'Z'),
+            length=14
+        )
+        )
+        if not rsi_indicator:
+            print('*instrument', instrument)
+            print('*rsi.text', rsi_indicator[0].text)
+            print('*******************')
+            return
+        # print(instrument)
+        # print(json.loads(rsi[0].text)['technicalIndicators'])
+        if res := json.loads(rsi_indicator[0].text)['technicalIndicators']:
+            if int(res[-1]['signal']['units']) > 70:
+                print(instrument)
+                await sender(instrument['ticker'])
+                # return res
 
 
 async def rsi():
     instruments: list = await get_instruments()
-    for instrument in instruments:
-        if instrument['ticker'] in settings.excluded_instruments:
-            return
-        result = await is_overbought(instrument)
-
-
-        if result:
-            print(instrument)
-            print(result)
-            await sender(instrument['ticker'])
+    semaphore = asyncio.Semaphore(20)
+    coro = (is_overbought(instrument, semaphore)
+            for instrument in  instruments
+            if instrument['ticker'] not in  settings.excluded_instruments)
+    await asyncio.gather(*coro)
+    # for instrument in instruments:
+    #     result = None
+    #
+    #     if instrument['ticker'] in settings.excluded_instruments:
+    #         pass
+    #     else:
+    #         result = await is_overbought(instrument)
+    #
+    #
+    #     if result:
+    #         print(instrument)
+    #         print(result)
+    #         await sender(instrument['ticker'])
 
 
 async def main():
--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 excluded_instruments = []
 excluded_days = ['6', '7']
-start_hour = 7
+start_hour = 4
 finish_hour = 20
